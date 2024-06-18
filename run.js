6ece567090c157cef8302aeee6352bdb9573b2ff
Farouq Jouti <farouq.jouti@rystadenergy.com>
2024-06-18 15:32:35 +0200

    feat: output is now json formatted

--- run.js
+++ run.js
@@ -8,8 +8,8 @@ const { Parser } = require('node-sql-parser');
 
 ( async () => {
 let distance, steps, path; 
-[distance, steps, path] = await SQLQueryDistance.parseAndCalculateDistance(process.argv[2], process.argv[3]);
-//console.timeEnd();
-console.log(SQLQueryDistance.stringifyDistance(distance, steps, path));
-console.log(distance)
+[distance, steps , ] = await SQLQueryDistance.parseAndCalculateDistance(process.argv[2], process.argv[3]);
+[worst_distance, , ] = await SQLQueryDistance.parseAndCalculateDistance(process.argv[2], undefined);
+	let output = {"distance": distance, "worst_distance": worst_distance, "steps": steps}
+console.log(JSON.stringify(output))
 })()
