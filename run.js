81b155c06b0368355c69e5a61365c3b11d9d058c
Farouq Jouti <farouq.jouti@rystadenergy.com>
2024-06-21 17:11:18 +0200

    fix: handle empty query strings as input

--- run.js
+++ run.js
@@ -5,12 +5,19 @@ const { Parser } = require('node-sql-parser');
 
 
 
+let destination_query = (process.argv[2] == '') ? undefined: process.argv[2] 
+let start_query = (process.argv[3] == '') ? undefined: process.argv[3] 
+if (destination_query == undefined){ 
+    let output = {"distance": 0, "worst_distance": 0, "steps": undefined}
+    console.log(output)
+    process.exit(0)
+}
 
 
 ( async () => {
-let distance, steps, path; 
-[distance, steps , ] = await SQLQueryDistance.parseAndCalculateDistance(process.argv[2], process.argv[3]);
-[worst_distance, , ] = await SQLQueryDistance.parseAndCalculateDistance(process.argv[2], undefined);
+let distance, steps; 
+[distance, steps , ] = await SQLQueryDistance.parseAndCalculateDistance(destination_query, start_query);
+[worst_distance, , ] = await SQLQueryDistance.parseAndCalculateDistance(destination_query, undefined);
 let output = {"distance": distance, "worst_distance": worst_distance, "steps": steps}
 console.log(JSON.stringify(output))
 })()
